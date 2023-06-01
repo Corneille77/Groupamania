175c42b732791e2998d67df905e631d057e78df4
Kokou Corneille D'ALMEIDA <101010758+Corneille77@users.noreply.github.com>
2023-06-01 11:45:48 +0200

    Revert "Nouveau dossier controllers, routes et réinstallation du package.json"

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const mysql = require("mysql");
 const dotenv = require('dotenv')
-const app = express();
-const userRoutes = require('./routes/user.routes').router
 
 dotenv.config({path: './.env'})
 
+const app = express();
 app.use(express.json())
 
 // Routes
-app.use('/api', userRoutes)
+app.use('/api/user', userRoutes)
 
-
-
-// Port du server
-app.listen(3030, () => {
-    console.log("Lectur du server sur le port 3030")
-});
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER, 
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
